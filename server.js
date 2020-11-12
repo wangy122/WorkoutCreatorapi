b43179f52cbd1f45bf872cbfcf591fbecd9dfbcb
wangy122 <54949529+wangy122@users.noreply.github.com>
2020-11-12 07:10:08 -0500

    Update with Member table

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('Groups'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('Members');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
