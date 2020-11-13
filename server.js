460e7c50f138b51247fb3c5e4637ba0e89248270
wangy122 <54949529+wangy122@users.noreply.github.com>
2020-11-13 15:18:10 -0500

    Add table users

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 
 mobileApp.tables.add('Groups'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('Members');
+mobileApp.tables.add('Users');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
